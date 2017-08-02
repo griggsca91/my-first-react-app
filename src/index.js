0b6ad5e701c2208cc9cbe98b4d70b97148743283
Chris G <griggsca91@gmail.com>
2017-08-02 19:03:08 -0400

    made a component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+
+class Welcome extends React.Component {
+    render() {
+        return <h1>Hello, {this.props.name}</h1>
+    }
+}
+
+const element = <Welcome name="Sara" />;
+
+    ReactDOM.render(
+        element,
+        document.getElementById('root')
+    );
+
