30500cba297477e8c0160f660021539dfdca7142
Chris G <griggsca91@gmail.com>
2017-08-02 19:31:04 -0400

    Statefulness and lifecycle

--- src/index.js
+++ src/index.js
@@ -3,19 +3,56 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
 
 
-
-class Welcome extends React.Component {
     render() {
-        return <h1>Hello, {this.props.name}</h1>
+        return (
+            <div>
+                <h1>Hello, world1</h1>
+                <h2>It is {this.state
+                        .date
+                        .toLocaleTimeString()}.</h2>
+            </div>
+        );
     }
 }
 
-const element = <Welcome name="Sara" />;
+class App extends React.Component {
+    render() {
+        return (<div>
+            <Clock />
+            <Clock />
+            <Clock />
+            </div>)
+    }
+}
 
+
+function tick() {
     ReactDOM.render(
-        element,
-        document.getElementById('root')
-    );
+        <App />, document.getElementById('root'));
+}
 
+setInterval(tick, 1000);
